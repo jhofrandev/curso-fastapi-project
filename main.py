0076a12d54f6d645398e5def364e6eda72f37dce
Jhofran Briceño <jhofran.dev@outlook.com>
2025-04-06 13:04:26 -0500

    feat: implement database setup and CRUD operations for customers

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import zoneinfo
 
-from fastapi import FastAPI
 from datetime import datetime
+from sqlmodel import select
+from fastapi import FastAPI, HTTPException, status
 
-from models import Customer, CustomerCreate, Transaction, Invoice
+from db import SessionDep, create_all_table
+from models import Customer, CustomerCreate, CustomerUpdate, Transaction, Invoice
 
 
-app = FastAPI()
+app = FastAPI(lifespan=create_all_table)
 
 country_timezones = {
   'CO': 'America/Bogota',
@@ -32,20 +34,60 @@ async def time(iso_code: str):
   tz = zoneinfo.ZoneInfo(timezone_str)
   return {'time': datetime.now(tz)}
 
-@app.post('/customers', response_model=Customer)
-async def create_customer(customer_data: CustomerCreate):
+@app.post(
+    '/customers', response_model=Customer, status_code=status.HTTP_201_CREATED
+)
+async def create_customer(customer_data: CustomerCreate, session: SessionDep):
   customer = Customer.model_validate(customer_data.model_dump())
-  customer.id = len(db_customers)
-  db_customers.append(customer)
+  session.add(customer)
+  session.commit()
+  session.refresh(customer)
   return customer
 
-@app.get('/customers', response_model=list[Customer])
-async def get_customers():
-  return db_customers
-  
 @app.get('/customers/{customer_id}', response_model=Customer)
-async def get_customer(customer_id: int):
-  return db_customers[customer_id]
+async def read_customer(customer_id: int, session: SessionDep):
+  customer_db = session.get(Customer, customer_id)
+  if not customer_db:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found"
+    )
+  return customer_db
+
+@app.patch(
+    '/customers/{customer_id}', 
+    response_model=Customer, 
+    status_code=status.HTTP_201_CREATED
+)
+async def update_customer(
+  customer_id: int, customer_data: CustomerUpdate, session: SessionDep
+):
+  customer_db = session.get(Customer, customer_id)
+  if not customer_db:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found"
+    )
+  customer_data_dict = customer_data.model_dump(exclude_unset=True)
+  customer_data.sqlmodel_update(customer_data_dict)
+  session.add(customer_db)
+  session.commit()
+  session.refresh(customer_db)
+  return customer_db
+
+@app.delete('/customers/{customer_id}')
+async def delete_customer(customer_id: int, session: SessionDep):
+  customer_db = session.get(Customer, customer_id)
+  if not customer_db:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found"
+    )
+  session.delete(customer_db)
+  session.commit()
+  return {'detail': 'ok'}
+
+@app.get('/customers', response_model=list[Customer])
+async def list_customers(session: SessionDep):
+  return session.exec(select(Customer)).all()
+  
 
 @app.post('/transactions')
 async def create_transaction(transaction_data: Transaction):
--- models.py
+++ models.py
@@ -1,21 +1,25 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
 
 
-class CustomerBase(BaseModel):
-  name: str
-  description: str | None
-  email: str
-  age: int
+class CustomerBase(SQLModel):
+  name: str = Field(default=None)
+  description: str | None = Field(default=None)
+  email: str = Field(default=None)
+  age: int = Field(default=None)
 
 class CustomerCreate(CustomerBase):
   pass
 
-class Customer(CustomerBase):
-  id: int | None = None
+class CustomerUpdate(CustomerBase):
+  pass
+
+class Customer(CustomerBase, table=True):
+  id: int | None = Field(default=None, primary_key=True)
 
 class Transaction(BaseModel):
-  id: int
+  id: int 
   ammount: int
   description: str
 
