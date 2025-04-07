002bf1fa0ef2bcf6092e1ccef8e8153fed1fcdc6
Jhofran Briceño <jhofran.dev@outlook.com>
2025-04-07 12:48:25 -0500

    feat: implement transaction creation and listing endpoints with validation

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,12 +1,34 @@
-from fastapi import  APIRouter
+from fastapi import  APIRouter, HTTPException, status
 
-
-from models import Transaction
+from db import SessionDep
+from sqlmodel import select
+from models import Customer, Transaction, TransactionCreate
 
 
 router = APIRouter(tags = ['transactions'])
 
 
-@router.post('/transactions')
-async def create_transaction(transaction_data: Transaction):
-  return transaction_data
+@router.post('/transactions', status_code=status.HTTP_201_CREATED)
+async def create_transaction(
+  transaction_data: TransactionCreate, session: SessionDep
+):
+  transaction_data_dict = transaction_data.model_dump()
+  customer = session.get(Customer, transaction_data_dict.get('customer_id'))
+  if not customer:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND, detail='Customer not found'
+    )
+  
+  transaction_db = Transaction.model_validate(transaction_data_dict)
+  session.add(transaction_db)
+  session.commit()
+  session.refresh(transaction_db)
+
+  return transaction_db
+
+@router.get('/transactions')
+async def list_transactions(session: SessionDep):
+  query = select(Transaction)
+  transactions = session.exec(query).all()
+  return transactions
+  
